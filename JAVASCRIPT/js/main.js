40b4f36e47b29abff181dc36cdf000fe72c3f496
Pranjal <pranjalgain052@gmail.com>
2020-08-16 22:04:23 +0530

    object added

--- JAVASCRIPT/js/main.js
+++ JAVASCRIPT/js/main.js
@@ -5,6 +5,7 @@ let a = 10;
 a = 40;
 const b = 20;
 console.log(c, a, b);
+
 //Data types String, Numbers, Boolean, Null, Undefined;
 const name = 'Pranjal';
 const age = 22;
@@ -16,3 +17,29 @@ console.log(typeof age);
 console.log(typeof status);
 console.log(typeof g);
 console.log(typeof x);
+
+//Array
+console.log(name.split('a'));
+console.log(name[3]);
+const arr = ['Hi', 'Hello'];
+arr.push('Bye');
+arr.unshift('Tata');
+arr[0] = 'a';
+arr[5] = 'b';
+arr.pop();
+console.log(arr);
+console.log(Array.isArray(arr));
+console.log(arr.indexOf('Hi'));
+console.log(arr.indexOf(''));
+
+//Objects
+const ob = {
+    name: name,
+    age: age,
+    status: status,
+    like: {
+        hobby: ['Football', 'Carrom', 'Photography'],
+    },
+    hi: 'a'
+}
+console.log(ob.like.hobby[2])
